844aaf2e02ad894d13b8ee102ca9b95829e6a259
uzn1023 <e5305@gm.kochi-ct.jp>
2024-07-21 08:57:41 +0900

    feat: タイムアウト延長

--- paper_summarizer/add_notion.py
+++ paper_summarizer/add_notion.py
@@ -14,125 +14,133 @@ def add_notion(dict, database_id,NOTION_API_KEY):
         database_id=database_id,
         filter={"property": "DOI", "rich_text": {"equals": dict['DOI']}}
     )
+
+    new_page_data = {
+        "parent": {"database_id": database_id},
+        "properties": {
+            "Name": {
+                "title": [
+                    {
+                        "text": {
+                            "content": dict['title']
+                        }
+                    }
+                ]
+            },
+            "authors": {
+                "multi_select": dict['authors']
+            },
+            "出版日": {
+                "date":{
+                    "start": dict["publish_date"]
+                }
+            },
+            "DOI": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": dict['DOI']
+                        }
+                    }
+                ]
+            },
+            "どんなものか？": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": dict['どんなものか？']
+                        }
+                    }
+                ]
+            },
+            "どこがすごい？": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": dict["どこがすごい？"]
+                        }
+                    }
+                ]
+            },
+            "肝となる手法は？": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": dict['肝となる手法は？']
+                        }
+                    }
+                ]
+            },
+            "どう主張が示された？": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": dict['どう主張が示された？']
+                        }
+                    }
+                ]
+            },
+            "残された課題は？": {
+                "rich_text": [
+                    {
+                        "text": {
+                            "content": dict['残された課題は？']
+                        }
+                    }
+                ]
+            },
+            "論文のキーワード": {
+                "multi_select": dict['論文のキーワード']
+            }
+        },
+        "children": [
+            {
+                "object": "block",
+                "type": "code",
+                "code": {
+                    "caption": [],
+                    "rich_text": [{
+                        "type": "text",
+                        "text": {
+                            "content": dict['mindmap'][:2000]
+                        }
+                    }],
+                    "language": "mermaid"
+                }
+            }
+        ]
+        +
+        [ 
+            {
+                "object": "block",
+                "type": "bulleted_list_item",
+                "bulleted_list_item": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": text[:2000]
+                            }
+                        } 
+                    ]
+                }
+            } for text in dict['各章要約']
+        ]
+    }
+    
     # DOI が存在しない場合のみ新しいページを作成
     if len(existing_pages['results']) == 0:
         # 新しいページを追加するデータ
-        new_page_data = {
-            "parent": {"database_id": database_id},
-            "properties": {
-                "Name": {
-                    "title": [
-                        {
-                            "text": {
-                                "content": dict['title']
-                            }
-                        }
-                    ]
-                },
-                "authors": {
-                    "multi_select": dict['authors']
-                },
-                "出版日": {
-                    "date":{
-                        "start": dict["publish_date"]
-                    }
-                },
-                "DOI": {
-                    "rich_text": [
-                        {
-                            "text": {
-                                "content": dict['DOI']
-                            }
-                        }
-                    ]
-                },
-                "どんなものか？": {
-                    "rich_text": [
-                        {
-                            "text": {
-                                "content": dict['どんなものか？']
-                            }
-                        }
-                    ]
-                },
-                "どこがすごい？": {
-                    "rich_text": [
-                        {
-                            "text": {
-                                "content": dict["どこがすごい？"]
-                            }
-                        }
-                    ]
-                },
-                "肝となる手法は？": {
-                    "rich_text": [
-                        {
-                            "text": {
-                                "content": dict['肝となる手法は？']
-                            }
-                        }
-                    ]
-                },
-                "どう主張が示された？": {
-                    "rich_text": [
-                        {
-                            "text": {
-                                "content": dict['どう主張が示された？']
-                            }
-                        }
-                    ]
-                },
-                "残された課題は？": {
-                    "rich_text": [
-                        {
-                            "text": {
-                                "content": dict['残された課題は？']
-                            }
-                        }
-                    ]
-                },
-                "論文のキーワード": {
-                    "multi_select": dict['論文のキーワード']
-                }
-            },
-            "children": [
-                {
-                    "object": "block",
-                    "type": "code",
-                    "code": {
-                        "caption": [],
-                        "rich_text": [{
-                            "type": "text",
-                            "text": {
-                                "content": dict['mindmap'][:2000]
-                            }
-                        }],
-                        "language": "mermaid"
-                    }
-                }
-            ]
-            +
-            [ 
-                {
-                    "object": "block",
-                    "type": "bulleted_list_item",
-                    "bulleted_list_item": {
-                        "rich_text": [
-                            {
-                                "type": "text",
-                                "text": {
-                                    "content": text[:2000]
-                                }
-                            } 
-                        ]
-                    }
-                } for text in dict['各章要約']
-            ]
-        }
-
         # Notionのデータベースに新しいページを追加
         response = notion.pages.create(**new_page_data)
-
+    else:
+        # 既存のページを更新
+        page_id = existing_pages['results'][0]['id']
+        response = notion.pages.update(
+            page_id=page_id,
+            properties=new_page_data['properties'],
+            children=new_page_data['children']
+        )
     # レスポンスを表示
     #print(response)
 
