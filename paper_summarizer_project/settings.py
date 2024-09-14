0be57a5e4ceb6290a59260b04ba19fc08b60bdb7
uzn1023 <e5305@gm.kochi-ct.jp>
2024-09-14 17:39:26 +0900

    change: dbをsqliteに変更

--- paper_summarizer_project/settings.py
+++ paper_summarizer_project/settings.py
@@ -84,12 +84,17 @@ WSGI_APPLICATION = "paper_summarizer_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+# default_dburl = "sqlite:///" + str(BASE_DIR / "db.sqlite3")
+# DATABASES = {
+#     "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
+# }
 
 DATABASES = {
-    "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
